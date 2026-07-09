3957472b4c135b4d9d4a48be8339ce88eb8587f0
Aditya21-dev <adityadas0217@gmail.com>
2026-07-09 18:10:47 +0530

    save and retrive img from cloudinary

--- cloudniary/project/app/views.py
+++ cloudniary/project/app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import User
+
 
 # Create your views here.
+
+def form(req):
+
+    if req.method == "POST":
+
+        User.objects.create(
+            name=req.POST.get("name"),
+            email=req.POST.get("email"),
+            phone=req.POST.get("phone"),
+            address=req.POST.get("address"),
+            profile_img=req.FILES.get("image")
+        )
+
+        return redirect("home")
+    
+    return render(req , "form.html")
+
+
+def home(req):
+    users = User.objects.all()
+    return render(req , "home.html" , {"users":users})
