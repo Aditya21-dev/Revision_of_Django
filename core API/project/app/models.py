92a7a418e9226426f1a34a3e82b4517bd8cf403b
Aditya21-dev <adityadas0217@gmail.com>
2026-07-13 23:52:55 +0530

    revised  core api

--- core API/project/app/models.py
+++ core API/project/app/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
-class Student(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    city = models.CharField(max_length=100)
-    
+    brand = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
-    
